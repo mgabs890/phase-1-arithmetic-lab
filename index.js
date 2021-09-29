89afbb6e1c21c2477f7848e8a04e17ccbd431598
Gabriella Delgado <m.gabriella.delgado@gmail.com>
2021-09-28 22:02:50 -0400

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+var num1=2;
+var num2=31;
+var multiply = num1 * num2;
+var random = Math.round(Math.random()*10)+1;
+var num3=18;
+var num4=7;
+var mod = num3%num4;
+var max = Math.max(15, 16, 18, 20);
